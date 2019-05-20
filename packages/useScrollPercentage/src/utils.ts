9ea38b66edea87a86c3fd3d02903c1e5f0254fbe
Daniel Schmidt <dsc@charlietango.dk>
2019-05-20 10:54:11 +0200

    fix: remove the magic 25% offset adjustment

--- packages/useScrollPercentage/src/utils.ts
+++ packages/useScrollPercentage/src/utils.ts
@@ -6,20 +6,11 @@ export function calculateVerticalPercentage(
   if (!root) return 0
   const vh =
     (root instanceof Element ? root.clientHeight : root.innerHeight) || 0
-  const offsetTop = threshold * vh * 0.25
-  const offsetBottom = threshold * vh * 0.25
+  const offset = threshold * vh
+  const percentage =
+    (bounds.bottom - offset) / (vh + bounds.height - offset * 2)
 
-  return (
-    1 -
-    Math.max(
-      0,
-      Math.min(
-        1,
-        (bounds.bottom - offsetTop) /
-          (vh + bounds.height - offsetBottom - offsetTop),
-      ),
-    )
-  )
+  return 1 - Math.max(0, Math.min(1, percentage))
 }
 
 export function calculateHorizontalPercentage(
@@ -29,18 +20,8 @@ export function calculateHorizontalPercentage(
 ) {
   if (!root) return 0
   const vw = (root instanceof Element ? root.clientWidth : root.innerWidth) || 0
-  const offsetLeft = threshold * vw * 0.25
-  const offsetRight = threshold * vw * 0.25
+  const offset = threshold * vw
+  const percentage = (bounds.right - offset) / (vw + bounds.width - offset * 2)
 
-  return (
-    1 -
-    Math.max(
-      0,
-      Math.min(
-        1,
-        (bounds.right - offsetLeft) /
-          (vw + bounds.width - offsetRight - offsetLeft),
-      ),
-    )
-  )
+  return 1 - Math.max(0, Math.min(1, percentage))
 }
