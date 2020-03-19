d0633a7659d3a1d7ea128c1f03680c09ddfc47b9
fbosch <frede.bosch@gmail.com>
2020-03-19 15:57:35 +0100

    fix unit test for useMouseClosestEdge

--- packages/useMouseClosestEdge/src/useMouseClosestEdge.test.js
+++ packages/useMouseClosestEdge/src/useMouseClosestEdge.test.js
@@ -2,7 +2,7 @@ import { renderHook } from '@testing-library/react-hooks'
 import useMouseClosestEdge from './useMouseClosestEdge'
 
 it('should execute the useMouseClosestEdge hook', () => {
-  const { result } = renderHook(() => useMouseDirection())
+  const { result } = renderHook(() => useMouseClosestEdge())
   const [ refSetter, position ] = result.current
   expect(refSetter).toBeInstanceOf(Function)
   expect(position).toHaveProperty('x')
