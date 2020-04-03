4460f02b3f475a7dd58d9eab7b147bc6a6eebee6
Daniel Schmidt <3764345+thebuilder@users.noreply.github.com>
2020-04-03 23:26:39 +0200

    Update useClientHydrated.ts

--- packages/useClientHydrated/src/useClientHydrated.ts
+++ packages/useClientHydrated/src/useClientHydrated.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 let clientHydrated = false
 
 /**
- * Returns false when serverside rendering nad during the first render pass (hydration) in the client.
+ * Returns false when serverside rendering and during the first render pass (hydration) in the client.
  * Use this to modify behavior of components when they can be certain they are running client side.
  * Like check a media query during the initial render.
  * */
