714f32be7ef9406b2c41362fdff337a027d27027
Daniel Schmidt <dsc@charlietango.dk>
2024-07-24 11:59:21 +0200

    only test esm files

--- .size-limit.ts
+++ .size-limit.ts
@@ -3,23 +3,15 @@ import { globbySync } from "globby";
 import type { SizeLimitConfig } from "size-limit";
 
 // Get all hooks from the `src/hooks` directory, and validate their size
-const importNames = globbySync("src/hooks/*.ts").map((file) => {
-  return path.parse(file).name;
-});
+const limits = globbySync("src/hooks/*.ts").map((file) => {
+  const name = path.parse(file).name;
 
-const limits = [
-  ...importNames.map((name) => ({
-    name: `${name} (ems)`,
+  return {
+    name: name,
     import: `{ ${name} }`,
-    limit: "1 kb",
     path: "dist/index.js",
-  })),
-  ...importNames.map((name) => ({
-    name: `${name} (cjs)`,
-    import: `{ ${name} }`,
-    limit: "1.5 kb",
-    path: "dist/index.cjs",
-  })),
-] satisfies SizeLimitConfig;
+    limit: "1 KB",
+  };
+}) satisfies SizeLimitConfig;
 
 export default limits;
